efa8805ae0dbdb0e16a5b752ce0408e05e273755
circlespainter <fabio@paralleluniverse.co>
2016-06-09 12:18:14 +0300

    Upgrade to Quasar 0.7.5, remove AoT and insert scheduling troubleshooting printouts

--- sync-examples/src/main/java/io/vertx/example/sync/fiberhandler/Server.java
+++ sync-examples/src/main/java/io/vertx/example/sync/fiberhandler/Server.java
@@ -1,55 +1,83 @@
 package io.vertx.example.sync.fiberhandler;
 
+import co.paralleluniverse.fibers.Fiber;
+import co.paralleluniverse.fibers.FiberForkJoinScheduler;
 import co.paralleluniverse.fibers.Suspendable;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.Message;
 import io.vertx.example.util.Runner;
+import io.vertx.ext.sync.Sync;
 import io.vertx.ext.sync.SyncVerticle;
+
+import java.io.PrintStream;
+import java.lang.reflect.Field;
+
 import static io.vertx.ext.sync.Sync.*;
 
 /**
- *
- *
- *
  * @author <a href="http://tfox.org">Tim Fox</a>
  */
 public class Server extends SyncVerticle {
+    private static final String ADDRESS = "some-address";
+    private static final PrintStream o = System.out;
 
-  private static final String ADDRESS = "some-address";
+    private static final String FIBER_SCHEDULER_CONTEXT_KEY;
 
+    static {
+        final Field f;
+        try {
+            f = Sync.class.getDeclaredField("FIBER_SCHEDULER_CONTEXT_KEY");
+            f.setAccessible(true);
+            FIBER_SCHEDULER_CONTEXT_KEY = (String) f.get(null);
+        } catch (final NoSuchFieldException | IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-  /*
-  Convenience method so you can run it in your IDE
-  Note if in IDE need to edit run settings and add:
-  -javaagent:/home/tim/.m2/repository/co/paralleluniverse/quasar-core/0.7.2/quasar-core-0.7.2.jar
-   */
-  public static void main(String[] args) {
-    Runner.runExample(Server.class);
-  }
+    /*
+    Convenience method so you can run it in your IDE
+    Note if in IDE need to edit run settings and add something like:
+    -javaagent:/Users/fabio/.m2/repository/co/paralleluniverse/quasar-core/0.7.5/quasar-core-0.7.5-jdk8.jar
+     */
+    public static void main(String[] args)  {
+        Runner.runExample(Server.class);
+    }
 
-  @Suspendable
-  @Override
-  public void start() throws Exception {
+    @Suspendable
+    @Override
+    public void start() throws Exception {
+        final EventBus eb = vertx.eventBus();
 
-    EventBus eb = vertx.eventBus();
-    eb.consumer(ADDRESS).handler(msg -> {
-      System.out.println("Waiting");
-      // reply after one second
-      vertx.setTimer(1000, tid -> msg.reply("wibble"));
-    });
+        // Replace fiber scheduler with actual one (e.g. ForkJoin). If you comment this out you'll see the "direct/inline"
+        // trace, where the fiber is started and executed in the same thread.
+        vertx.getOrCreateContext().put(FIBER_SCHEDULER_CONTEXT_KEY, new FiberForkJoinScheduler("vertx-sync", Runtime.getRuntime().availableProcessors()));
 
-    // If you want to do sync stuff in an async handler it must be transformed to a fiber handler
-    vertx.createHttpServer().requestHandler(fiberHandler(req -> {
+        eb.consumer(ADDRESS).handler(msg -> {
+            o.println("[VertxHandler] StackTrace:");
+            new Throwable().printStackTrace(o);
+            final Object vertxSyncFiberSchedulerAttrValue = vertx.getOrCreateContext().get(FIBER_SCHEDULER_CONTEXT_KEY);
+            printScheduler("[VertxHandler] ", vertxSyncFiberSchedulerAttrValue);
+            o.println("[VertxHandler] replying");
+            msg.reply("");
+        });
 
-      // Send a message to address and wait for a reply
-      Message<String> reply = awaitResult(h -> eb.send(ADDRESS, "blah", h));
+        // If you want to do sync stuff in an async handler it must be transformed to a fiber handler
+        vertx.createHttpServer().requestHandler(fiberHandler(req -> {
+            o.println("[HTTPReqHandler] StackTrace:");
+            new Throwable().printStackTrace(o);
+            // Send a message to address and wait for a reply
+            final Message<String> reply = awaitResult(h -> eb.send(ADDRESS, "blah", h));
+            final Object vertxSyncFiberSchedulerAttrValue = vertx.getOrCreateContext().get(FIBER_SCHEDULER_CONTEXT_KEY);
 
-      System.out.println("Got reply: " + reply.body());
+            printScheduler("[HTTPReqHandler] ", vertxSyncFiberSchedulerAttrValue);
+            o.println("[HTTPReqHandler] Got reply.");
 
-      req.response().end("blah");
+            req.response().end("Check stdout!");
+        })).listen(8080, "localhost");
+    }
 
-    })).listen(8080, "localhost");
-
-  }
-
+    private static void printScheduler(String prefix, Object vertxSyncFiberSchedulerAttrValue) {
+        o.println(prefix + "vertx-sync FIBER_SCHEDULER_CONTEXT_KEY attribute value: " + (vertxSyncFiberSchedulerAttrValue != null ? vertxSyncFiberSchedulerAttrValue.getClass().getName() : "<NOT SET>"));
+        o.println(prefix  + (Fiber.currentFiber() == null ? "Not running in a fiber" : "Running in a fiber, scheduler class: " + Fiber.currentFiber().getScheduler().getClass().getName()));
+    }
 }
